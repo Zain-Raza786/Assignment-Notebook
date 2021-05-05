43556505383aaa50af2993553eb589647b8ed4a5
Zain Raza <zainoakgrove@gmail.com>
2021-05-05 15:09:17 -0500

    Restored files

---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,32 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Student on 4/12/21.
+//
+
+import Foundation
+
+
+class AssignmentList: ObservableObject {
+    @Published var items : [Assignment] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Assignment].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
+    
+}
--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -7,10 +7,41 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddAssignmentView = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentList.items.remove(atOffsets: indexSet)
+                })
+            }
+            .sheet(isPresented: $showingAddAssignmentView, content: {
+                AddAssignmentView(assignmentList: assignmentList)
+            })
+            .navigationBarTitle("Assignment List")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                                                    showingAddAssignmentView = true }) {
+                                    Image(systemName: "plus")
+                                })
+        }
     }
 }
 
@@ -19,3 +50,16 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Assignment: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
+
+
+
+
+
+
